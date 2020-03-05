6bb0f32bc8d84ac39b7f9664c819565651e60aa3
LunaOlbrechts <luna.olbrechts@gmail.com>
2020-03-05 08:30:19 +0100

    ES6 create class Note

--- lab3/notes app/src/app.js
+++ lab3/notes app/src/app.js
@@ -1,2 +1,69 @@
-var nums = [1,2,3];
-var doubleNums = nums.map((e) => e * 2);
+class Note {
+    constructor(title) {
+      this.title = title;
+      // HINT🤩 this.element = this.createElement(title);
+
+    }
+    
+    createElement(title){
+        let newNote = document.createElement('div');
+        let remove_btn= document.getElementsByClassName("btnAddNote");
+        remove_btn.addEventListener('click', this.remove.bind(newNote));
+        console.log(haha);
+        return newNote;
+    }
+    
+    add(){
+      // HINT🤩
+      // this function should append the note to the screen somehow
+      let ul= document.getElementsByClassName("card");
+      let li= document.createElement("li");
+      li.appendChild(document.createTextNode("Four"));
+    }
+    
+    saveToStorage(){
+      // HINT🤩
+      // localStorage only supports strings, not arrays
+      // if you want to store arrays, look at JSON.parse and JSON.stringify
+    }
+    
+    remove(){
+      // HINT🤩 the meaning of 'this' was set by bind() in the createElement function
+      // in this function, 'this' will refer to the current note element
+    } 
+  }
+  
+  class App {
+    constructor() {
+      console.log("👊🏼 The Constructor!");
+    
+      // HINT🤩
+      // clicking the button should work
+      // pressing the enter key should also work
+      // this.btnAdd = ???
+      // this.btnAdd.addEventListener("click", this.createNote.bind(this));
+      // this.loadNotesFromStorage();
+    }
+    
+    loadNotesFromStorage() {
+      // HINT🤩
+      // load all notes from storage here and add them to the screen
+      // something like note.add() in a loop would be nice
+    }
+     
+    createNote(e){
+      // this function should create a new note by using the Note() class
+      
+      // HINT🤩
+      // note.add();
+      // note.saveToStorage();
+      // this.reset();
+    }
+    
+    reset(){
+      // this function should reset the form 
+    }
+    
+  }
+  
+  let app = new App();
