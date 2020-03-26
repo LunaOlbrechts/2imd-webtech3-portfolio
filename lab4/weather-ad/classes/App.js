9d4ab3ad15f275cf55b0eeed8c03faca81d92e64
LunaOlbrechts <luna.olbrechts@gmail.com>
2020-03-26 13:33:04 +0100

    Added new class Advertisement to change the add based on the weather

--- lab4/weather-ad/classes/App.js
+++ lab4/weather-ad/classes/App.js
@@ -1,45 +1,77 @@
-class App{
-    constructor(){
+class App {
+    constructor() {
         this.getLocation();
         this.lat;
         this.lng;
     }
 
-    getLocation(){
+    getLocation() {
         navigator.geolocation.getCurrentPosition(
-            this.gotLocation.bind(this), 
+            this.gotLocation.bind(this),
             this.errorLocation.bind(this)
-            );
+        );
     }
 
-    gotLocation(result){
+    gotLocation(result) {
         this.lat = result.coords.latitude;
         this.lng = result.coords.longitude;
+
         this.getWeather();
     }
 
-    getWeather(){
-        let url =  `https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/8ec81cb7b4e5a14b12eefbeb0526f793/${this.lat},${this.lng}?units=si`;
-        
+    getWeather() {
+        let url = `https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/8ec81cb7b4e5a14b12eefbeb0526f793/${this.lat},${this.lng}?units=si`;
+
         fetch(url)
             .then(response => {
                 return response.json();
             })
             .then(data => {
-                document.querySelector(".weatherSummary").innerHTML = data.currently.summary;
-                document.querySelector(".weatherTemp").innerHTML = data.currently.temperature + " C°";
+                let sum = data.currently.summary;
+                let temp = data.currently.temperature;
+                let adverstisement = new Adverstisement(sum, temp);
+
+                console.log(sum, temp);
             })
             .catch(err => {
                 console.log(err);
             });
     }
 
-    errorLocation(err){
+    errorLocation(err) {
         console.log(err);
     }
-
-    
 }
 
 let app = new App();
 
+class Adverstisement {
+    constructor(sum, temp) {
+        this.changeBackground();
+        this.changeFooter();
+        sum;
+        temp;
+    }
+
+    changeBackground(sum) {
+        let url = "";
+        let background = document.querySelector(".background");
+        switch(sum) {
+            case "clear":
+                url = "../images/plants.jpg";
+                break;
+            case "Drizzle": 
+                url = "../images/plants.jpg";
+                break;
+            default:
+                url = "../images/plants.jpg";
+          }
+        
+          background.style.backgroundImage = url;
+    }
+
+    changeFooter(sum, temp) {
+        document.querySelector(".weatherSummary").innerHTML = sum;
+        document.querySelector(".weatherTemp").innerHTML = temp + " C°";
+    }
+}
